f6cd001fdf15f609122d199242166ea3e2fa194f
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-16 21:18:19 +0200

    Add functionality for listing products

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 
 
 function App() {
+  const [products, setProducts] = useState([
+    {name: 'product1', price: 100.00},
+    {name: 'product2', price: 200.00},
+
+  ]);
   return (
-    <div className="App">
+    <div>
     <h1>Store</h1>
+    <ul>
+    {products.map((item, index) =>(
+      <li key={index}>{item.name} - {item.price}</li>
+    ))}
+    </ul>
     </div>
   );
 }
