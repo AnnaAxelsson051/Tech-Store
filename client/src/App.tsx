6276b44ed142088520348909629eccd92e68094a
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-16 21:30:37 +0200

    Add functionality for adding products

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,8 +5,13 @@ function App() {
   const [products, setProducts] = useState([
     {name: 'product1', price: 100.00},
     {name: 'product2', price: 200.00},
-
   ]);
+
+  function addProduct(){
+    setProducts(prevState => [...prevState, 
+      {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}])
+  }
+
   return (
     <div>
     <h1>Store</h1>
@@ -15,6 +20,7 @@ function App() {
       <li key={index}>{item.name} - {item.price}</li>
     ))}
     </ul>
+    <button onClick={addProduct}>Add Product</button>
     </div>
   );
 }
