0c6a922248320b0070115963b7921db87b28f60d
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-19 14:41:20 +0200

    Update navbar add shopping cart icon

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,4 +1,5 @@
-import { AppBar, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 const midLinks =[
@@ -21,7 +22,10 @@ export default function Header({darkMode, handleThemeChange}: Props){
     return(
         <AppBar position='static' sx={{mb: 4}}>
             <Toolbar>
-               <Typography variant='h6'>
+               <Typography variant='h6' component={NavLink} 
+               to='/'
+               sx={{color: 'inherit', textDecoration: 'none'}}
+               >
                  <h1>TECH STORE</h1>
                </Typography>
                <Switch checked={darkMode} onChange={handleThemeChange}/>
@@ -37,6 +41,13 @@ export default function Header({darkMode, handleThemeChange}: Props){
                     </ListItem>
                 ))}
             </List>
+
+<IconButton size='large' edge='start' color='inherit' sx={{mr: 2}}>
+<Badge badgeContent='4' color='secondary' >
+    <ShoppingCart/>
+</Badge>
+</IconButton>
+
             <List sx={{display: 'flex'}}>
                 {rightLinks.map(({title, path}) => (
                     <ListItem
