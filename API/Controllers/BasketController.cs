f529eccfbe4a67bcba94de53ebf5361e93e465a2
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-20 15:57:43 +0200

    Add functionality for saving basket with a global unique identifyer

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -18,15 +18,47 @@ namespace API.Controllers
            [HttpGet]
            public async Task<ActionResult>Basket>> GetBasket()
            {
-            var basket = await _context.Baskets
-            .Include(in => int.Items)
-            .ThenInclude(p => p.Product) //info
-            .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+            
+           var basket = await RetrieveBasket();
            
            if (basket == null) return NotFound();
 
            return basket;
            }
-        
+
+           [HttpPost]
+           public async Task <ActionResult> AddItemToBasket(int productId, int quantity)
+           {
+            var basket = await RetrieveBasket();
+            if (basket == null) basket = CreateBasket();
+
+            return StatusCode(201);
+           }
+
+
+           [HttpDelete]
+           public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
+           {
+            return Ok();
+           }
+             private async Task <BasketController> RetrieveBasket()
+           {
+            return await _context.Baskets
+            .Include(in => int.Items)
+            .ThenInclude(p => p.Product) //info
+            .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+           }
+
+        //Creating a basket with a global unique identifyer for the basket from db
+        //Adding basket
+        private Basket CreateBasket()
+           {
+            var buyerId = Guid.NewGuid().ToString();
+            var cookieOptions = new CookieOptions{IsEssential = true, Expires = DateTime.Now.AddDays(30)};
+            Response.Cookies.Append("buyerId", buyerId, cookieOptions)
+          var basket = new Basket{BuyerId = buyerId};
+          _context.Baskets.Add(basket);
+          return basket;
+          }
     }
 }
