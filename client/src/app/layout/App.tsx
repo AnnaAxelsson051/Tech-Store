bac1fd70ba738c2ca87df5ea9e8a433853e94df6
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-21 14:21:53 +0200

    Add functionality for getting customer basket with cookies

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,12 +2,31 @@
 import Catalog from "../../features/catalog/Catalog";
 import {Container, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import Header from "./Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { useStoreContext } from "../context/StoreContext";
+import { getCookie } from "../util/util";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
+  const {setBasket} = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  //Getting basket based on cookie, getting basket from api
+  //catching error, turning of loading
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if (buyerId){
+      agent.Basket.get()
+      .then(basket => setBasket(basket))
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false));
+    }
+  }, [setBasket])
+
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark' : 'light';
  const theme = createTheme({
@@ -23,6 +42,8 @@ function App() {
  function handleThemeChange(){
   setDarkMode(!darkMode);
  }
+
+ if (loading) return <LoadingComponent message="Initializing app..."/>
 
   return (
     <ThemeProvider theme={theme}>
---
+++ client/src/app/util/util.ts
@@ -0,0 +1,6 @@
+//Cookie functionality for basket
+
+export function getCookie(key: string){
+    const b = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
+    return b ? b.pop() : "";
+}
