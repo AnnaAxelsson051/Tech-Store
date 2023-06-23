7daa998ecf4561b83e326a0be8c1e05deafd135e
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-23 10:14:13 +0200

    Update fetch product functionality

--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -16,6 +16,17 @@ return await agent.Catalog.list();
     }
 )
 
+export const fetchProductAsync = createAsyncThunk<Product, number>(
+    'catalog/fetchProductsAsync',
+    async (productId) => {
+        try {
+return await agent.Catalog.details(productId)
+        } catch (error){
+            console.log(error);
+        }
+    }
+)
+
 export const catalogSlice = createSlice ({
     name: 'catalog',
     initialState: productsAdapter.getInitialState({
@@ -34,7 +45,17 @@ export const catalogSlice = createSlice ({
         });
         builder.addCase(fetchProductsAsync.rejected, (state) => {
             state.status = 'idle';
-        })
+        });
+        builder.addCase(fetchProductAsync.pending, (state) => {
+            state.status = 'pendingFetchProduct';
+        });
+        builder.addCase(fetchProductAsync.fulfilled, (state, action) => {
+            productsAdapter.upsertOne(state, action.payload);
+            state.status = 'idle';
+        });
+    builder.addCase(fetchProductAsync.rejected, (state) => {
+        state.status = 'idle';
+    })
     })
 })
 export const productSelectors = productsAdapter.getSelectors((state: RootState) => state.catalog);
