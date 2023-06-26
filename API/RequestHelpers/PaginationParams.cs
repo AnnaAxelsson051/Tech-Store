17ac9a7ee4f4e3339a6a0225810944c3dfeb3ad5
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-26 16:55:16 +0200

    Add helper classes for pagination

---
+++ API/RequestHelpers/PaginationParams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace API.RequestHelpers
+{
+	public class PaginationParams
+	{
+
+		private const int MaxPageSize = 50;
+
+		public int PageNumber { get; set; } = 1;
+
+		private int _pageSize = 6; //default pg size
+
+		public int PageSize
+		{
+			get => _pageSize;
+			set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+		}
+	}
+}
+
