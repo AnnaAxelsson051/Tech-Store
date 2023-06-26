11f079bb620a96302145717c37870529562c429a
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-26 14:45:23 +0200

    Modify errorhandeling for reducers

--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -7,11 +7,11 @@ const productsAdapter = createEntityAdapter <Product>();
 
     export const fetchProductsAsync = createAsyncThunk<Product[]>(
     'catalog/fetchProductsAsync',
-    async () => {
+    async (_, thunkAPI) => {
         try {
 return await agent.Catalog.list();
-        } catch (error){
-            console.log(error);
+        } catch (error: any){
+            return thunkAPI.rejectWithValue({error: error.data});
         }
     }
 )
@@ -43,7 +43,8 @@ export const catalogSlice = createSlice ({
             state.status = 'idle';
             state.productsLoaded = true;
         });
-        builder.addCase(fetchProductsAsync.rejected, (state) => {
+        builder.addCase(fetchProductsAsync.rejected, (state, action) => {
+            console.log(action.payload);
             state.status = 'idle';
         });
         builder.addCase(fetchProductAsync.pending, (state) => {
