899e4e66226534397787ace03662930fe7a70045
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-28 18:50:14 +0200

    Add accountcontroller class

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using API.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Controllers
+{
+	public class AccountController : BaseApiController
+     {
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager)
+		{
+		_userManager = userManager;
+		}
+	}
+}
+
