e88bc776aac74f37dc8db585bac4282e5b3a86bd
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-28 15:30:14 +0200

    Add skeleton component for loading

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -3,7 +3,7 @@ import ProductList from "./ProductList";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
 import { fetchFilters, fetchProductsAsync, productSelectors, setPageNumber, setProductParams } from "./catalogSlice";
-import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, Grid, Pagination, Paper, Radio, RadioGroup, Typography } from "@mui/material";
+import {  Grid, Paper } from "@mui/material";
 import ProductSearch from "./ProductSearch";
 import RadioButtonGroup from "../../app/components/RadioButtonGroup";
 import CheckboxButtons from "../../app/components/CheckboxButtons";
@@ -17,7 +17,7 @@ const sortOptions = [
 
 export default function Catalog() {
     const products = useAppSelector(productSelectors.selectAll);
-    const { productsLoaded, status, filtersLoaded, brands, types, productParams, metaData } = useAppSelector(state => state.catalog);
+    const { productsLoaded, filtersLoaded, brands, types, productParams, metaData } = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
 
     //Setting the products
@@ -30,7 +30,7 @@ export default function Catalog() {
         if (!filtersLoaded) dispatch(fetchFilters());
     }, [dispatch, filtersLoaded])
 
-    if (status.includes('pending') || !metaData) return <LoadingComponent message='Loading products...' />
+    if (!filtersLoaded) return <LoadingComponent message='Loading products...' />
 
     return (
         <>
--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,22 +1,29 @@
 import { Grid, List } from "@mui/material";
 import { Product } from "../../app/models/product";
 import ProductCard from "./ProductCard";
+import { useAppSelector } from "../../app/store/configureStore";
+import ProductCardSkeleton from "./ProductCardSkeleton";
 
 
 interface Props {
     products: Product[];
 }
 
-export default function ProductList({products}: Props){
+export default function ProductList({ products }: Props) {
+    const { productsLoaded } = useAppSelector(state => state.catalog);
     return (
-    <Grid container spacing={4}>
-    {products.map(product =>(
-        //6 = two products on each row
-        //4 = three products on each row
-        <Grid item xs={6} key={product.id}>
-  <ProductCard product ={product}/>
-  </Grid>
-    ))}
-    </Grid>
+        <Grid container spacing={4}>
+            {products.map(product => (
+                //6 = two products on each row
+                //4 = three products on each row
+                <Grid item xs={6} key={product.id}>
+                    {!productsLoaded ? (
+                        <ProductCardSkeleton />
+                    ) : (
+                        <ProductCard product={product} />
+                    )}
+                </Grid>
+            ))}
+        </Grid>
     )
 }
