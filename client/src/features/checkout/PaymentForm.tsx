57e45f8489b6337a0899b98c88ec49a3f8e51ebf
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-05 15:17:12 +0200

    Update payment form

--- client/src/features/checkout/PaymentForm.tsx
+++ client/src/features/checkout/PaymentForm.tsx
@@ -1,6 +1,9 @@
 import { Typography, Grid, TextField, FormControlLabel, Checkbox } from "@mui/material";
+import { useFormContext } from "react-hook-form";
+import AppTextInput from "../../app/components/AppTextInput";
 
 export default function PaymentForm() {
+    const {control} = useFormContext();
   return (
     <>
       <Typography variant="h6" gutterBottom>
@@ -8,18 +11,12 @@ export default function PaymentForm() {
       </Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cardName"
-            label="Name on card"
-            fullWidth
-            autoComplete="cc-name"
-            variant="standard"
-          />
+         <AppTextInput
+         name='nameOnCard' label='Name on card' control={control}/>
         </Grid>
         <Grid item xs={12} md={6}>
           <TextField
-            required
+            //required
             id="cardNumber"
             label="Card number"
             fullWidth
@@ -29,7 +26,7 @@ export default function PaymentForm() {
         </Grid>
         <Grid item xs={12} md={6}>
           <TextField
-            required
+            // required
             id="expDate"
             label="Expiry date"
             fullWidth
@@ -39,7 +36,7 @@ export default function PaymentForm() {
         </Grid>
         <Grid item xs={12} md={6}>
           <TextField
-            required
+            // required
             id="cvv"
             label="CVV"
             helperText="Last three digits on signature strip"
--- client/src/features/checkout/checkoutValidation.ts
+++ client/src/features/checkout/checkoutValidation.ts
@@ -1,6 +1,7 @@
 import * as yup from 'yup';
 
-export const validationSchema = yup.object({
+export const validationSchema = [
+    yup.object({
     fullName: yup.string().required('Full name is required'),
     address1: yup.string().required('Address line 1 is required'),
     address2: yup.string().required('Address line 2 is required'),
@@ -8,4 +9,9 @@ export const validationSchema = yup.object({
     state: yup.string().required('State is required'),
     zip: yup.string().required('Zip is required'),
     country: yup.string().required('Country is required'),
+}),
+yup.object(),
+yup.object({
+    nameOnCard: yup.string().required()
 })
+]
