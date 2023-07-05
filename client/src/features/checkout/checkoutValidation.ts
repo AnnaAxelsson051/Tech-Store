f63daa01ba2432de62ee6af3269582baae56f182
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-05 15:04:19 +0200

    Modify review component

--- client/src/features/checkout/checkoutValidation.ts
+++ client/src/features/checkout/checkoutValidation.ts
@@ -4,8 +4,8 @@ export const validationSchema = yup.object({
     fullName: yup.string().required('Full name is required'),
     address1: yup.string().required('Address line 1 is required'),
     address2: yup.string().required('Address line 2 is required'),
-    city: yup.string().required('Address line 1 is required'),
-    state: yup.string().required('Address line 1 is required'),
-    zip: yup.string().required('Address line 1 is required'),
-    country: yup.string().required('Address line 1 is required'),
+    city: yup.string().required('City 1 is required'),
+    state: yup.string().required('State is required'),
+    zip: yup.string().required('Zip is required'),
+    country: yup.string().required('Country is required'),
 })
