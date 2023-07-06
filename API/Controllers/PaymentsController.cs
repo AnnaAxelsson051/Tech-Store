6838559a3d475476cadbad23d2dc46d9e2551b2b
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-06 22:59:19 +0200

    Add stripe webhook

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using API.Data;
 using API.DTOs;
+using API.Entities.OrderAggregate;
 using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stripe;
 
 namespace API.Controllers
 {
 	public class PaymentsController : BaseApiController
 	{
 		private readonly PaymentService _paymentService;
-		public PaymentsController(PaymentService paymentService, StoreContext context)
+        private readonly StoreContext _context;
+        private readonly IConfiguration _config;
+
+        public PaymentsController(PaymentService paymentService, StoreContext context, IConfiguration config)
 		{
+			_config = config;
+			_context = context;
 			_paymentService = paymentService;
 		}
 
@@ -32,6 +40,23 @@ namespace API.Controllers
 			basket.ClientSecret = basket.ClientSecret ?? intent.ClientSecret;
 			if (!result) return BadRequest(new ProblemDetails { Title = "Problem updating basket with intent" });
 			return basket.MapBasketToDto();
+		}
+
+		[HttpPost("webhook")]
+		public async Task<ActionResult>StripeWebHook()
+		{
+			var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+			var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"],
+				_config["StripeSettings:WhSecret"]);
+
+			var charge = (Charge)stripeEvent.Data.Object;
+			var order = await _context.Orders.FirstOrDefaultAsync(x =>
+			x.PaymentIntentId == charge.PaymentIntentId);
+
+			if (charge.Status == "succeeded") order.OrderStatus = OrderStatus.PaymentReceived;
+			await _context.SaveChangesAsync();
+
+			return new EmptyResult();
 		}
 	}
 }
