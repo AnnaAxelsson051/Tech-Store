1e85ddf36250ba9cdcf865d55cd9ab3e9e69775e
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-07 15:36:43 +0200

    Add update product functionality

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -74,6 +74,17 @@ namespace API.Controllers
 			if (result) return CreatedAtRoute("GetProduct", new { Id = product.Id }, product);
 			return BadRequest(new ProblemDetails { Title = "Problem creating new product" });
 		}
+
+		[Authorize(Roles = "Admin")]
+		[HttpPut]
+		public async Task<ActionResult> UpdateProduct(UpdateProductDto productDto)
+		{
+			var product = await _context.Products.FindAsync(productDto.Id);
+			if (product == null) return NotFound();
+			_mapper.Map(productDto, product);
+			var result = await _context.SaveChangesAsync() > 0;
+			if (result) return NoContent();
+		}
     }
 }
 
