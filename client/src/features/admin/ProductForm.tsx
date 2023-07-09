4b3309862a9ef084e9e2035809e5f407398a0c15
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-09 18:19:50 +0200

    Add styling to image drop zone

--- client/src/features/admin/ProductForm.tsx
+++ client/src/features/admin/ProductForm.tsx
@@ -1,10 +1,11 @@
 import { Typography, Grid, Paper, Box, Button } from "@mui/material";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import AppTextInput from "../../app/components/AppTextInput";
 import { Product } from "../../app/models/product";
 import { useEffect } from "react";
 import useProducts from "../../app/hooks/useProducts";
 import AppSelectList from "../../app/components/AppSelectList";
+import AppDropZone from "../../app/components/AppDropZone";
 
 interface Props {
     product?: Product;
@@ -12,18 +13,25 @@ interface Props {
 }
 
 export default function ProductForm({product, cancelEdit}: Props) {
-    const { control, reset } = useForm();
+    const { control, reset, handleSubmit, watch } = useForm();
     const {brands, types}= useProducts();
+    const watchFile = watch('file', null);
 
     useEffect (() => {
         if (product) reset(product);
     }, [product, reset])
+
+    function handleSubmitData(data: FieldValues) {
+        console.log(data);
+
+    }
 
     return (
         <Box component={Paper} sx={{p: 4}}>
             <Typography variant="h4" gutterBottom sx={{mb: 4}}>
                 Product Details
             </Typography>
+            <form onSubmit={handleSubmit(handleSubmitData)}>
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={12}>
                     <AppTextInput control={control} name='name' label='Product name' />
@@ -44,13 +52,21 @@ export default function ProductForm({product, cancelEdit}: Props) {
                     <AppTextInput multiline={true} rows={4} control={control} name='description' label='Description' />
                 </Grid>
                 <Grid item xs={12}>
-                    <AppTextInput control={control} name='pictureUrl' label='Image' />
+                    <Box display = 'flex' justifyContent='space-between' alignItems='center'>
+                        <AppDropZone control={control} name='file' />
+                    {watchFile ? (
+                        <img src={watchFile.preview} alt="preview" style={{maxHeight: 200}}/>
+                    ) : (
+                        <img src={product?.pictureUrl} alt={product?.name} style={{maxHeight: 200}}/>
+                    )}
+                    </Box>
                 </Grid>
             </Grid>
             <Box display='flex' justifyContent='space-between' sx={{mt: 3}}>
                 <Button onClick={cancelEdit} variant='contained' color='inherit'>Cancel</Button>
-                <Button variant='contained' color='success'>Submit</Button>
+                <Button type='submit' variant='contained' color='success'>Submit</Button>
             </Box>
+            </form>
         </Box>
     )
 }
